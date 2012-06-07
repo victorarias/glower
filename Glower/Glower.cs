76991e572cf6da35e807ce2b7854371c2992fdaa
Victor Arias <arias.sp@gmail.com>
2012-06-07 19:07:06 -0300

    pushing first version

---
+++ Glower/Glower.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlowerFramework
+{
+    /// <summary>
+    /// A micro IoC/DI framework
+    /// </summary>
+    public class Glower
+    {
+        private Dictionary<Type, Func<object>> providers = new Dictionary<Type, Func<object>>();
+
+        public void Bind<TKey, TImpl>() where TImpl : TKey
+        {
+            providers.Add(typeof(TKey), () => Resolve<TImpl>());
+        }
+
+        public T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        private object Resolve(Type type)
+        {
+            Func<object> provider;
+
+            if (providers.TryGetValue(type, out provider))
+            {
+                return provider();
+            }
+            else
+            {
+                return ResolveByGreediestConstructor(type);
+            }
+        }
+
+        private object ResolveByGreediestConstructor(Type type)
+        {
+            var constructor = type.GetConstructors().OrderBy(a => a.GetParameters().Length).First();
+            var parameters = constructor.GetParameters();
+
+            if (parameters.Length == 0)
+                return Activator.CreateInstance(type);
+            else
+            {
+                var resolvedParameters = from p in parameters
+                                         select Resolve(p.ParameterType);
+
+                return constructor.Invoke(resolvedParameters.ToArray());
+            }
+        }
+
+        public void Bind<T>(T instance)
+        {
+            providers[typeof(T)] = () => instance;
+        }
+    }
+}
---
+++ Tests/GlowerTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GlowerFramework;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GlowerTests
+    {
+        [Test]
+        public void ShouldBindGenericTypes()
+        {
+            var glower = new Glower();
+
+            glower.Bind<IFooDependency, FooDependency>();
+
+            var resolved = glower.Resolve<IFooDependency>();
+            resolved.Should().Not.Be.Null();
+            resolved.Should().Be.OfType<FooDependency>();
+        }
+
+        [Test]
+        public void ShouldResolveTypesWithoutDependencies()
+        {
+            var glower = new Glower();
+
+            var resolved = glower.Resolve<FooDependency>();
+
+            resolved.Should().Not.Be.Null();
+        }
+
+        [Test]
+        public void ShouldResolveTypeWithDependencies()
+        {
+            var glower = new Glower();
+
+            glower.Bind<IFooDependency, FooDependency>();
+            glower.Bind<INestedDependency, NestedDependency>();
+            glower.Bind<IAnotherFooDependency, AnotherFooDependency>();
+
+            var foo = glower.Resolve<Foo>();
+
+            foo.Should().Not.Be.Null();
+        }
+
+        [Test]
+        public void ShouldResolveBindedInstance()
+        {
+            var glower = new Glower();
+            var instance = new FooDependency();
+
+            glower.Bind(instance);
+
+            var resolved = glower.Resolve<FooDependency>();
+
+            resolved.Should().Be.SameInstanceAs(instance);
+        }
+
+        interface IFoo { void DoSomething(); }
+        class Foo : IFoo
+        {
+            public Foo(IFooDependency dependency, IAnotherFooDependency anotherDependency)
+            {
+                if (null == dependency) throw new ArgumentNullException();
+                if (null == anotherDependency) throw new ArgumentNullException();
+            }
+
+            public void DoSomething()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        interface IFooDependency { void DoSomethingDifferent(); }
+        public class FooDependency : IFooDependency
+        {
+            public void DoSomethingDifferent()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        interface INestedDependency { }
+        public class NestedDependency : INestedDependency { }
+
+        interface IAnotherFooDependency { void Blofs();}
+        class AnotherFooDependency : IAnotherFooDependency
+        {
+            public AnotherFooDependency(INestedDependency nestedDependency)
+            {
+                if (null == nestedDependency) throw new ArgumentNullException();
+            }
+            public void Blofs()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+    }
+}
